fb7ace2d1130f0ef4be99437dee026ce4b8c0477
aazim19 <86780135+aazim19@users.noreply.github.com>
2026-04-25 18:33:54 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/macrosync/', // This must match your GitHub repository name exactly
-  logLevel: 'error', 
+  base: '/macrosync/', // ADD THIS LINE
+  logLevel: 'error',
   plugins: [
     base44({
       legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
